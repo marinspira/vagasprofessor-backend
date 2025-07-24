7f090ef9673a9ca8882940c55b72a568aa8edb63
Maria Eduarda <marinspiraa@gmail.com>
2025-07-24 16:39:46 -0300

    Create role validation

--- src/main/java/com/vagas_professor/dto/auth/RegisterRequest.java
+++ src/main/java/com/vagas_professor/dto/auth/RegisterRequest.java
@@ -2,6 +2,7 @@ package com.vagas_professor.dto.auth;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 
 // Record is a special class projected to be a transparent "container" for imatables data
 public record RegisterRequest(
@@ -16,5 +17,6 @@ public record RegisterRequest(
         String password,
 
         @NotBlank(message = "Role is required")
+        @Pattern(regexp = "SCHOOL|TEACHER", message = "Role must be either SCHOOL or TEACHER")
         String role
         ) {}
